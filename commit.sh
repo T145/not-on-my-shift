37ae101e2eea78c3fdbfd49cd3cf3fe661ae467e
Marcos Del Sol Vives <marcos@orca.pet>
2020-05-05 16:20:28 +0200

    Change commit.sh so only domains are commited

--- commit.sh
+++ commit.sh
@@ -5,12 +5,6 @@ set -o pipefail
 
 cd "$(dirname "$0")"
 
-changed=$(git status --porcelain | wc -l)
-if [ $changed -eq 0 ]; then
-	echo "No differences - nothing to do"
-	exit 0
-fi
-
 countdiff() {
 	local file="$1"
 	local type="$2"
@@ -25,7 +19,6 @@ countdiff() {
 added=$(countdiff domains.txt +)
 deleted=$(countdiff domains.txt -)
 
-logmsg="Random changes"
 if [ $added -gt 0 ]; then
 	if [ $deleted -gt 0 ]; then
 		logmsg="Added $added and deleted $deleted domains"
@@ -34,8 +27,11 @@ if [ $added -gt 0 ]; then
 	fi
 elif [ $deleted -gt 0 ]; then
 	logmsg="Deleted $deleted domains"
+else
+	echo "No changes to domains - aborting"
+	exit 0
 fi
 
-git add .
+git add domains.txt
 git commit -m "$logmsg"
 git push
