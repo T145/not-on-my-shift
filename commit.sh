51572d367955c76587ac43e78725b0616107f9ea
Marcos Del Sol Vives <marcos@orca.pet>
2020-05-25 20:10:46 +0200

    Updated commit script

--- commit.sh
+++ commit.sh
@@ -5,33 +5,10 @@ set -o pipefail
 
 cd "$(dirname "$0")"
 
-countdiff() {
-	local file="$1"
-	local type="$2"
-
-	expr \
-		$(git diff          "$file" | tail -n +5 | grep ^\\${type}[^#] | wc -l) + \
-		$(git diff --cached "$file" | tail -n +5 | grep ^\\${type}[^#] | wc -l)
-
-	return 0
-}
-
-added=$(countdiff domains.txt +)
-deleted=$(countdiff domains.txt -)
-
-if [ $added -gt 0 ]; then
-	if [ $deleted -gt 0 ]; then
-		logmsg="Added $added and deleted $deleted domains"
-	else
-		logmsg="Added $added domains"
-	fi
-elif [ $deleted -gt 0 ]; then
-	logmsg="Deleted $deleted domains"
+git add filters.yml
+if git diff-index --cached --quiet HEAD; then
+	echo "Nothing to do"
 else
-	echo "No changes to domains - aborting"
-	exit 0
+	git commit -m "Updated filters"
+	git push
 fi
-
-git add domains.txt
-git commit -m "$logmsg"
-git push
