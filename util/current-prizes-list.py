86825f8ffb20fa4d1d97983f84367fb37a1bcccc
Marcos Del Sol Vives <marcos@orca.pet>
2020-10-23 10:22:43 +0200

    Improve prize search script

--- util/current-prizes-list.py
+++ util/current-prizes-list.py
@@ -3,39 +3,35 @@
 import adutil
 import requests
 import re
+import os
+import sys
+import yaml
+import socket
+from requests.exceptions import ConnectionError
 
-ips = [
-	'5.188.178.19',
-	'5.188.178.180',
-	'5.188.178.120',
-	'62.138.18.107',
-	'85.25.210.155',
-	'193.35.50.251',
-]
+# Root path of the project
+noms_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+print('Project root: %s' % noms_path)
 
-printed = set()
+known_ips = set()
+with open(os.path.join(noms_path, 'util', 'prizes-ips.txt')) as f:
+	pending_ips = [x.strip() for x in f.read().strip().split()]
 
-def test_all(hostnames):
-	# Remove redirection targets
-	hostnames = [x for x in hostnames if not x.endswith('.live')]
+prizesdomains = None
+with open(os.path.join(noms_path, 'filters.yml')) as f:
+	filter_list = yaml.safe_load(f)
 
-	hostnames = sorted(hostnames)
-	for hostname in hostnames:
-		hostname = hostname.strip()
-		if hostname in printed:
-			continue
+known_domains = None
+for group in filter_list['groups'].values():
+	for entry in group['entries']:
+		if entry.get('prizesdomains'):
+			known_domains = set(entry['domains'])
 
-		try:
-			if adutil.is_prize_domain(hostname):
-				printed.add(hostname)
-				print(hostname)
-		except Exception:
-			pass
+if not known_domains:
+	print('Could not find current prize domain list', file=sys.stderr)
+	sys.exit(1)
 
-for ip in ips:
-	print('# IP %s' % ip)
-	vault_reply = requests.get('https://otx.alienvault.com/otxapi/indicator/IPv4/passive_dns/%s' % ip).json()
-	test_all([entry['hostname'] for entry in vault_reply['passive_dns']])
+print('Previously known: %d' % len(known_domains))
 
 def matches_ad_pattern(x):
 	if re.match(r'^[a-z-]{15,30}[0-9]+\.life$', x):
@@ -46,8 +42,49 @@ def matches_ad_pattern(x):
 
 	return False
 
-recent = set(adutil.list_new_domains())
-recent = filter(matches_ad_pattern, recent)
+pending_domains = set(adutil.list_new_domains())
+pending_domains = set(filter(matches_ad_pattern, pending_domains))
+pending_domains.update(known_domains)
+tested_domains = set(known_domains)
 
-print('# Recently registered domains matching')
-test_all(recent)
+while len(pending_ips) > 0 or len(pending_domains) > 0:
+	print('=== NEW ITERATION ===')
+	print('--- Pending IPs: %d' % len(pending_ips))
+	for ip in pending_ips:
+		print(' - IP %s' % ip)
+		vault_reply = requests.get('https://otx.alienvault.com/otxapi/indicator/IPv4/passive_dns/%s' % ip).json()
+		for entry in vault_reply['passive_dns']:
+			pending_domains.add(entry['hostname'])
+	pending_ips = set()
+
+	# Remove redirection targets
+	print('--- Pending domains: %d' % len(pending_domains))
+	hostnames = [x for x in sorted(pending_domains) if not x.endswith('.live')]
+	for hostname in hostnames:
+		hostname = hostname.strip()
+		if hostname in tested_domains:
+			continue
+		tested_domains.add(hostname)
+
+		try:
+			print('Test %s' % hostname)
+			if adutil.is_prize_domain(hostname):
+				known_domains.add(hostname)
+				print(hostname)
+				for ip in socket.gethostbyname_ex(hostname)[2]:
+					if ip not in known_ips:
+						print('Adding new IP: %s' % ip)
+						pending_ips.add(ip)
+						known_ips.add(ip)
+		except ConnectionError:
+			pass
+
+	pending_domains = set()
+
+print('=== FINAL LIST OF DOMAINS ===')
+for domain in sorted(known_domains):
+	print('              - %s' % domain)
+
+with open(os.path.join(noms_path, 'util', 'prizes-ips.txt'), 'w') as f:
+	for ip in sorted(known_ips):
+		print(ip, file=f)
