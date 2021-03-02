5ecec3ff2fafa097b60da45c3fedac67e84208e9
Marcos Del Sol Vives <marcos@orca.pet>
2021-03-02 17:42:01 +0100

    Allow domains to leave the blacklist on Fedex

--- tools/update-fedex-list.py
+++ tools/update-fedex-list.py
@@ -9,44 +9,72 @@ import adutil
 import os
 import yaml
 from urllib.parse import urlsplit
+from kill_timeout import kill_timeout
 
 print('Begin')
 
+class NotHostingException(Exception):
+	pass
+
 with open(os.path.join(adutil.project_root, 'filters', 'fedex.yml')) as f:
 	saved_data = yaml.safe_load(f)
-	apkhost = set(saved_data['domains'])
+	apkhost = saved_data['domains']
 
-host_valid = ['amirapache.ir', 'contornosdesign.pt']
+suffixes = ['web', 'pkg', 'app', 'fedex']
 client = requests.Session()
+client.timeout = 5
 client.headers['User-Agent'] = 'Mozilla/5.0 (Android 10; Mobile; rv:85.0) Gecko/85.0 Firefox/85.0'
 
+@kill_timeout(20)
+def get_link_for_domain(prev_domain):
+	for suffix in suffixes:
+		print('Trying %s suffix %s' % (prev_domain, suffix))
+		reply = client.get('http://' + prev_domain + '/' + suffix + '/?sdeasdefsa').text
+		if 'rastrear su paquete' not in reply:
+			continue
+
+		reply = lxml.html.fromstring(reply)
+		link = reply.xpath("//a[starts-with(@href, 'http')]")[0]
+		if 'Descargar aplicación' not in link.text_content():
+			continue
+
+		return link.attrib['href']
+
+	return None
+
+def run_iter(prev_domain):
+	download_link = get_link_for_domain(prev_domain)
+	if download_link is None:
+		raise NotHostingException('Domain ' + prev_domain + ' no longer hosting')
+
+	parsed_link = urlsplit(download_link)
+
+	new_domain = parsed_link.netloc
+	if new_domain.startswith('www.'):
+		new_domain = new_domain[4:]
+
+	if new_domain not in apkhost:
+		link_get = client.get(download_link)
+		if link_get.status_code == 200:
+			print('New link: ' + download_link)
+			apkhost.append(new_domain)
+		else:
+			print('Resolved nonworking link: ' + download_link)
+	else:
+		print('Nothing new')
+
 try:
-	while len(host_valid) > 0:
+	while len(apkhost) > 0:
+		prev_domain = random.choice(apkhost)
 		try:
-			prev_domain = random.choice(host_valid)
-			print('Trying with ' + prev_domain)
-			reply = client.get('http://' + prev_domain + '/pkg/').text
-			reply = lxml.html.fromstring(reply)
-			link = reply.xpath("//a[starts-with(@href, 'http')]")[0].attrib['href']
-			parsed_link = urlsplit(link)
+			run_iter(prev_domain)
 
-			new_domain = parsed_link.netloc
-			if new_domain.startswith('www.'):
-				new_domain = new_domain[4:]
-
-			if new_domain not in apkhost:
-				print(new_domain)
-				apkhost.add(new_domain)
-				host_valid.append(new_domain)
-
-				with open(os.path.join(adutil.project_root, 'filters', 'fedex.yml'), 'w') as f:
-					f.write("# Don't bother manually updating this file.\n")
-					f.write("# It is automatically updated with the tools/update-fedex-list.py script.\n")
-					yaml.dump({'domains': sorted(apkhost)}, f)
+		except NotHostingException as e:
+			print('No longer using ' + prev_domain)
+			apkhost.remove(prev_domain)
 
 		except Exception as e:
 			print('Failed using ' + prev_domain + ': ' + str(e))
-			host_valid.remove(prev_domain)
 
 		time.sleep(10)
 except KeyboardInterrupt:
