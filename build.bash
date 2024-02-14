46251909b55227601ae3be44f51694963375a250
T145 <T145@protonmail.com>
2024-02-14 13:17:24 -0500

    Fixed CI

--- build.bash
+++ build.bash
@@ -1,24 +1,12 @@
 #!/bin/bash
 
 set -euo pipefail
-cd $(dirname "$0")
-
-# Create environment if it does not exist
-if [ ! -d venv ]; then
-	virtualenv -p python3 venv
-
-	# Activate it
-	source venv/bin/activate
-else
-	# If it exists, just activate it
-	source venv/bin/activate
-fi
 
 # Install requirements
 pip install -r tools/requirements.txt
 
 # Create build directory
-[ -d built ] || mkdir built
+mkdir -p built
 
 # Finally build it
-python tools/build.py filters/main.yml --hosts built/hosts.txt --domains built/domains.txt --abp built/abp.txt
+python tools/build.py filters/main.yml --hosts built/hosts.txt --abp built/abp.txt --domains built/domains.txt
