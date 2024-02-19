ff76b3a1c0e8b5d5127a0bb653a8b7dceafc7afc
T145 <T145@protonmail.com>
2024-02-19 11:50:54 -0500

    Update only when the list content acutally changes

--- build.bash
+++ build.bash
@@ -7,5 +7,10 @@ pip install -r tools/requirements.txt
 #python tools/update-fedex-list.py
 python tools/update-mono-list.py
 python tools/update-prizes-list.py
-mkdir -p built
-python tools/build.py filters/main.yml --hosts built/hosts.txt --abp built/abp.txt --domains built/domains.txt
+
+if git diff-index --cached --quiet HEAD; then
+	echo "Nothing to do!"
+else
+	mkdir -p built
+    python tools/build.py filters/main.yml --hosts built/hosts.txt --abp built/abp.txt --domains built/domains.txt
+fi
