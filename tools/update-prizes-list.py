84e8ca2b6f12e4762370de8a4bd1b45419f982f7
T145 <T145@protonmail.com>
2024-02-20 13:28:40 -0500

    Fixed prize list error handling

--- tools/update-prizes-list.py
+++ tools/update-prizes-list.py
@@ -12,7 +12,7 @@ import glob
 import asyncio
 import aiohttp
 import time
-from aiohttp.client_exceptions import ClientConnectorError
+from aiohttp.client_exceptions import ClientResponseError
 
 
 HEADERS = {
@@ -21,20 +21,23 @@ HEADERS = {
 
 
 async def _check_host(domain, session):
-	print('Testing %s' % domain)
+	print(f'Testing {domain}')
 	try:
 		async with session.get(url=f'http://{domain}/', params={'u': 'tqck80z', 'o': 'zdqr96x', 't': 'DESKuniqANDsearch'}, headers=HEADERS) as response:
 			resp = await response.text()
 
 			if 'under construction' in resp.lower() or 'redirDomain' in resp:
-				print(f'NEW: {domain}')
+				print(f'ACTIVE: {domain}')
 				return domain
-	except ClientConnectorError as e:
-		print("Unable to get {} due to {}: it will be recorded as dead.".format(domain, e.__class__))
-		return f'-{domain}'
+
+			return None
 	except Exception as e:
-		print("Got recorded offender {} with error {}.".format(domain, e.__class__))
-		return domain
+		if isinstance(e, ClientResponseError) and (e.status == 400 or e.status == 404):
+			print(f'Unable to get {domain}: it will be recorded as dead.')
+			return f'-{domain}'
+		else:
+			print(f'Got recorded offender {domain} with error {e.__class__}.')
+			return domain
 
 
 async def check_hosts(domains):
@@ -43,19 +46,22 @@ async def check_hosts(domains):
 	async with aiohttp.ClientSession(timeout=timeout) as session:
 		ret = await asyncio.gather(*(_check_host(domain, session) for domain in domains))
 
-	active, inactive = list(), list()
+	active, inactive = set(), set()
 
 	for host in ret:
 		if host:
 			if host.startswith('-'):
-				inactive.append(host[1:])
+				inactive.add(host[1:])
 			else:
-				active.append(host)
+				active.add(host)
 
 	return active, inactive
 
 
 def matches_ad_pattern(x):
+	if x.endswith('.live'):
+		return False
+
 	if re.match(r'^[a-z-]{15,30}[0-9]+\.life$', x):
 		return True
 
@@ -65,39 +71,30 @@ def matches_ad_pattern(x):
 	return False
 
 
-def list_new_domains():
-	with open('nrd-list-downloader/nrd-2days-free.txt') as text:
-		return [domain.strip() for domain in text.readlines()]
-	return list()
-
-
 if __name__ == '__main__':
 	with open(os.path.join(os.getcwd(), 'filters', 'prizes.yml')) as f:
 		data = yaml.safe_load(f)
-		active_domains = data['domains']
-		inactive_domains = data['inactive_domains'] or list()
+		active_domains = set(data['domains'])
+		inactive_domains = set(data['inactive_domains']) or set()
 
 	print(f'Previously known: {len(active_domains)}')
-	pending_domains = set(filter(matches_ad_pattern, set(list_new_domains())))
+
+	with open('nrd-list-downloader/nrd-2days-free.txt') as text:
+		pending_domains = set(filter(matches_ad_pattern, set([domain.strip() for domain in text.readlines()])))
 
 	if len(pending_domains) > 0:
-		hostnames = [x for x in pending_domains if not x.endswith('.live')]
+		print(f'Pending domains: {len(pending_domains)}')
 
-		print(f'Pending domains: {len(hostnames)}')
-
-		hostnames.extend(active_domains)
-		hostnames.extend(inactive_domains)
+		pending_domains.union(active_domains)
+		pending_domains.union(inactive_domains)
 
 		start = time.time()
-		active_hosts, inactive_hosts = asyncio.run(check_hosts(hostnames))
+		active_hosts, inactive_hosts = asyncio.run(check_hosts(pending_domains))
 		end = time.time()
 
-		print("Took {} seconds to check {} domains.".format(end - start, len(hostnames)))
-
-		active_domains = active_hosts
-		inactive_domains = inactive_hosts
+		print("Took {} seconds to check {} domains.".format(end - start, len(pending_domains)))
 
 	with open(os.path.join(os.getcwd(), 'filters', 'prizes.yml'), 'w') as f:
 		f.write("# Don't bother manually updating this file.\n")
 		f.write("# It is automatically updated with the tools/update-prizes-list.py script.\n")
-		yaml.dump({'domains': sorted(set(active_domains)), 'inactive_domains': sorted(set(inactive_domains))}, f)
+		yaml.dump({'domains': sorted(active_hosts), 'inactive_domains': sorted(inactive_hosts)}, f)
